57d4310b070c541f31d72e42a3b529be693b5b5f
emmarahmawati <emmarahmawati08@guru.sma.belajar.id>
2026-07-20 19:07:54 +0900

    Delete js directory

--- js/config.js
+++
@@ -1,4 +0,0 @@
-const CONFIG = {
-  // Ganti teks di bawah dengan URL Web App milik Anda secara utuh
-  API_ENDPOINT: "https://script.google.com/macros/s/AKfycbzBTxkyE5EcFP95fTkb1y7ZEAyGPVRiKMz_0NiKMSqnCo6-nzbRkkImVIWvt76TlS1vZg/exec"
-};
